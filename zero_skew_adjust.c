070c800f7d709ce426c51c7d1d7e4383dd65e2fc
nik1106 <nik110695@gmail.com>
2018-11-20 14:13:45 -0500

    r and c are global variables, removed from function definition

--- zero_skew_adjust.c
+++ zero_skew_adjust.c
@@ -1,26 +1,32 @@
 #include "zst.h"
-void zero_skew_adjust(node* curr, double r, double c) {
+
+void zero_skew_adjust(node *curr) {
     double wire_delay_l = r * curr->left_wire_len * (curr->left->total_cap + c * curr->left_wire_len / 2);
     double wire_delay_r = r * curr->right_wire_len * (curr->right->total_cap + c * curr->right_wire_len / 2);
+
     double lmax = curr->left->max_delay + wire_delay_l;
     double lmin = curr->left->min_delay + wire_delay_l;
     double rmax = curr->right->max_delay + wire_delay_r;
     double rmin = curr->right->min_delay + wire_delay_r;
+
     if(rmax >= lmax && rmin <= lmin) {
         //Do nothing;
         return;
     }
+
     if(lmax >= rmax && lmin <= rmin) {
         //Do nothing;
         return;
     }
-    if(rmax-lmin >= lmax - rmin) {
-        double temp = rmax -lmin;
+
+    if(rmax - lmin >= lmax - rmin) {
+        double temp = rmax - lmin;
         if(temp > SKEW_BOUND) {
             //Adjust left
         }
     }
-    else if (rmax-lmin < lmax - rmin) {
+
+    else if (rmax - lmin < lmax - rmin) {
         double temp = lmax - rmin;
         if(temp > SKEW_BOUND) {
             //Adjust right
