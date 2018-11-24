11ceeadca6610041daf5b798accc0de2a91e9335
Wenbo Wei <wwei5743@gmail.com>
2018-11-24 15:10:46 -0500

    attempt to maintain zeroskew... Probably incorrect

--- zero_skew_adjust.c
+++ zero_skew_adjust.c
@@ -7,6 +7,33 @@
 
 #include "zst.h"
 
+void adjust_left(node* parent, double skew_diff) {
+    if(parent->left->node_num == -1) {
+        double a = - r * parent->left_wire_len * inv_cin;
+        double b = r * parent->left_wire_len + 
+                    0.69 * inv_rout * (parent->left->left->total_cap + c * parent->left->left_wire_len / 2- skew_diff);
+        double c  = -0.69 * inv_rout * (parent->left->left->total_cap + c * parent->left->left_wire_len / 2);
+        int n = (-b + sqrt(pow(b,2) - 4 * a * c)) / (2 * a);
+        if(n <= 0) {
+            perror("Invalid number of parallel inverters\n");
+        }
+        parent->left->num_node_inv = n;
+    }
+}
+void adjust_right(node* parent, double skew_diff) {
+    if(parent->right->node_num == -1) {
+        double a = - r * parent->right_wire_len * inv_cin;
+        double b = r * parent->right_wire_len + 
+                    0.69 * inv_rout * (parent->right->left->total_cap + c * parent->right->left_wire_len / 2- skew_diff);
+        double c  = -0.69 * inv_rout * (parent->right->left->total_cap + c * parent->right->left_wire_len / 2);
+        int n = (-b + sqrt(pow(b,2) - 4 * a * c)) / (2 * a);
+        if(n <= 0) {
+            perror("Invalid number of parallel inverters\n");
+        }
+        parent->right->num_node_inv = n;
+    }
+
+}
 void zero_skew_adjust(node *curr)
 {
     //if the current node is an inverter
@@ -14,6 +41,10 @@ void zero_skew_adjust(node *curr)
         double wire_delay_l = r * curr->left_wire_len * (curr->left->total_cap + c * curr->left_wire_len / 2);
         curr->min_delay = curr->left->min_delay + wire_delay_l;
         curr->max_delay = curr->left->max_delay + wire_delay_l;
+        return;
+    }
+    //if the current node is a sink node
+    else if(curr->leaf_node_label != -1) {
         return;
     }
     double wire_delay_l = r * curr->left_wire_len * (curr->left->total_cap + c * curr->left_wire_len / 2);
@@ -33,18 +64,25 @@ void zero_skew_adjust(node *curr)
         //Do nothing;
         return;
     }
-
+    if(lmax >= SINK_BOUND) {
+        adjust_left(curr, lmax-SINK_BOUND);
+    }
+    if(rmax >= SINK_BOUND) {
+        adjust_right(curr, rmax-SINK_BOUND);
+    }
     if(rmax - lmin >= lmax - rmin) {
         double temp = rmax - lmin;
+        //Right branch arrival too late, adjust right branch
         if(temp > SKEW_BOUND) {
-            //Adjust left
+            adjust_right(curr, temp);
         }
     }
 
     else if (rmax - lmin < lmax - rmin) {
         double temp = lmax - rmin;
+        //Left branch arrival too late, adjust left branch
         if(temp > SKEW_BOUND) {
-            //Adjust right
+            adjust_left(curr, temp);
         }
     }
 }
