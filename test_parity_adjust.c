0e1ec3f0d1b36991b64fde8f1f984e10f527d034
Wenbo Wei <wwei5743@gmail.com>
2018-11-23 19:42:09 -0500

    Parity adjustment

---
+++ test_parity_adjust.c
@@ -0,0 +1,34 @@
+#include "zst.h"
+void test_parity_adjust(node* curr, node* parent) {
+    if(curr == NULL) {
+        return;
+    }
+    if(curr->leaf_node_label != -1) {
+        if(curr->num_up_inv % 2 == 1) {
+            node* new_node = create_internal_node(-1, parent->left, parent->right);
+            new_node->x_coordinate = parent->x_coordinate;
+            new_node->y_coordinate = parent->y_coordinate;
+            new_node->left_wire_len = parent->left_wire_len;
+            parent->left_wire_len = 0.0;
+            new_node->right_wire_len = parent->right_wire_len;
+            parent->right_wire_len = -1.0;
+            new_node->num_node_inv = 1;
+            new_node->total_cap = parent->total_cap;
+            parent->total_cap = inv_cin;
+            if(parent->node_num == -1){
+                new_node->next = parent;
+                curr->next = new_node;
+            }
+            else{
+                while(curr->next != parent) {
+                    curr = curr->next;
+                }
+                curr->next = new_node;
+                new_node->next = parent;
+            }
+            parent->left = new_node;
+        }
+    }
+    test_parity_adjust(curr->left, curr);
+    test_parity_adjust(curr->right, curr);
+}
